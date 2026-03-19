a9d23d9b9489c658f1b84dab82c4b873890d944e
Spibb20 <spibb20@gmail.com>
2026-03-20 04:47:24 +0800

    test error fixed

--- apps/ebms/specs/index.spec.tsx
+++ apps/ebms/specs/index.spec.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Page from '../app/page';
+import { render, screen } from '@testing-library/react';
 
-describe('Page', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Page />);
-    expect(baseElement).toBeTruthy();
+jest.mock('@clerk/nextjs/server', () => ({
+  auth: jest.fn(async () => ({
+    isAuthenticated: true,
+  })),
+}));
+
+jest.mock('next/navigation', () => ({
+  redirect: jest.fn(),
+}));
+
+import Home from '../app/page';
+
+describe('Home', () => {
+  it('renders without crashing', async () => {
+    await Home();
+    expect(true).toBe(true);
   });
 });
