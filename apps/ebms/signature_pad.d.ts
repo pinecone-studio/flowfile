ff8b747cfea1f8ddafddc7f15486efc496745559
Zolo517 <191654686+Zolo517@users.noreply.github.com>
2026-03-20 00:56:30 +0800

    fix: eslint errors

---
+++ apps/ebms/signature_pad.d.ts
@@ -0,0 +1,19 @@
+declare module 'signature_pad' {
+  export type Options = {
+    backgroundColor?: string;
+    penColor?: string;
+    minWidth?: number;
+    maxWidth?: number;
+  };
+
+  export default class SignaturePad {
+    constructor(
+      canvas: HTMLCanvasElement,
+      options?: Options,
+    );
+
+    clear(): void;
+    isEmpty(): boolean;
+    off(): void;
+  }
+}
